6f4e4f51423cfac3858e8b8d47c5c439ee6bd048
Arhum farooq <arhummalavia123@gmail.com>
2024-07-23 20:15:14 +0500

    assignments

---
+++ assignments/main.dart
@@ -0,0 +1,22 @@
+void main(){
+  int num1 = 5;
+  int num2 = 6;
+  int num3 = 7;
+
+  if(num1>=num2){
+    print("num1 is greater") ;
+  }
+    else{
+
+      if(num2 >=num3){
+        print("num2 is greater");
+      }
+      else if (num3>=num1){
+        print("num3 is greater");
+
+      }
+      else {
+        print ("no one is greater or smaller");
+      }
+    }
+  }
